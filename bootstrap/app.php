7e749e6cae2d645342212c23511ec21cd14ea7a9
Happy Engineer <happyengineer63@gmail.com>
2024-03-20 20:18:54 +0800

    exception handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Exceptions\ValidationError;
 use App\Http\Middleware\LogMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(LogMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->dontReport(ValidationError::class);
+
+        $exceptions->renderable(function (ValidationError $exception, Request $request) {
+            return response("Bad request", 400);
+        });
     })->create();
